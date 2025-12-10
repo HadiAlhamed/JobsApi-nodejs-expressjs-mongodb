e2fa164353d63452daaa7e3d3db664b9b13ca31c
Hadi Alhamed <hadialhamed.py@gmail.com>
2025-12-10 13:21:03 +0300

    use bcrypt for hashing passwords

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,13 @@
+const bcrypt = require("bcryptjs");
+const User = require("../models/User");
+const {StatusCodes} = require("http-status-codes");
 const register = async(req , res)=>{
-        res.send("user register");
+        const {name , email , password} = body.req;
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password , salt);
+        const tempUser = {name , email , password:hashedPassword};
+        const user = await User.create({...tempUser}); 
+        res.status(StatusCodes.CREATED).json({user});
 };
 
 const login = async(req , res)=>{
