a3403ebdb244152163aaef455673d8a0089fcef3
Hadi Alhamed <hadialhamed.py@gmail.com>
2025-12-10 20:50:55 +0300

    generate token with jwt

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,10 +1,14 @@
-const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const User = require("../models/User");
 const {StatusCodes} = require("http-status-codes");
 const register = async(req , res)=>{
         
-        const user = await User.create({...req.body}); 
-        res.status(StatusCodes.CREATED).json({user});
+        const user = await User.create({...req.body});
+
+        const token = jwt.sign( {userId: user._id , name: user.name}, process.env.JWT_SECRET , {
+                expiresIn:'30d'
+        })
+        res.status(StatusCodes.CREATED).json({token , user : {name: user.name}});
 };
 
 const login = async(req , res)=>{
