53a02c5f735f0e0758e0edf743d3dbcfc62bc681
Hadi Alhamed <hadialhamed.py@gmail.com>
2025-12-10 21:24:47 +0300

    use Schema.methods for more organized code

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,9 +5,7 @@ const register = async(req , res)=>{
         
         const user = await User.create({...req.body});
 
-        const token = jwt.sign( {userId: user._id , name: user.name}, process.env.JWT_SECRET , {
-                expiresIn:'30d'
-        })
+        const token = user.createJWT();
         res.status(StatusCodes.CREATED).json({token , user : {name: user.name}});
 };
 
