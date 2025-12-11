f62b5b15df0c47fc5409412291509c23b0da5eca
Hadi Alhamed <hadialhamed.py@gmail.com>
2025-12-11 19:32:46 +0300

    add authMiddleware to check user token

---
+++ middleware/authentication.js
@@ -0,0 +1,37 @@
+const User = require("../models/User");
+const {UnauthenticatedError} = require("../errors");
+const jwt = require("jsonwebtoken");
+const { model } = require("mongoose");
+const authMiddleware = async(req , res , next)=>{
+        const authHeader = req.headers.authorization;
+        console.log(authHeader);
+        if(!authHeader || !authHeader.startsWith("Bearer "))
+        {
+                throw new UnauthenticatedError("Authentication invalid");
+        }
+        const token = authHeader.split(' ')[1];
+        try{
+                const decoded = jwt.verify(
+                        token,
+                        process.env.JWT_SECRET,
+                        {
+                                expiresIn: process.env.JWT_LIFETIME,
+                        }
+                );
+                //we signed with userId and name 
+                const {userId , name} = decoded;
+                const user = await User.findOne({_id:userId});
+                if(!user)
+                {
+                        throw new UnauthenticatedError("Invalid Credentials");
+                }
+                req.user = {userId , name}; 
+                next();
+        }catch(error)
+        {
+                throw new UnauthenticatedError("Invalid Credentials");
+        }
+
+};
+
+module.exports = authMiddleware;
