54f227dcf5dd8f0d5c632115f5b3fceb5a473819
Hadi Alhamed <hadialhamed.py@gmail.com>
2025-12-15 01:28:00 +0300

    handle Duplicate error , castError(notFound) and validation error

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,10 +1,29 @@
-const { CustomAPIError } = require("../errors");
 const { StatusCodes } = require("http-status-codes");
 const errorHandlerMiddleware = (err, req, res, next) => {
-  if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ message: err.message });
+  let customeError = {
+    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    message: err.message || 'Something went wrong try again later',
   }
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err });
+  if(err.name === 'CastError')
+  {
+    customeError.message = `No item fount with id : ${err.value}`; 
+    customeError.statusCode = 404;
+  }
+
+  if(err.name === 'ValidationError')
+  {
+    customeError.message = Object.values(err.errors).map((item)=>{
+      return item.message;
+    }).join(',');
+    customeError.statusCode = 400; 
+  }
+  if(err.code && err.code === 11000)
+  {
+    customeError.message = `Duplicate value entered for ${Object.keys(err.keyValue)} field , please chooose another value`;
+    customeError.statusCode = 400;
+  }
+  // return res.status(customeError.statusCode).json({err});
+  return res.status(customeError.statusCode).json({message:customeError.message});
 };
 
 module.exports = errorHandlerMiddleware;
