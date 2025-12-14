cf4f2733a283171412510d3318ea753b04df159b
Hadi Alhamed <hadialhamed.py@gmail.com>
2025-12-15 01:04:13 +0300

    bugfix : require->required typo

--- models/User.js
+++ models/User.js
@@ -4,13 +4,13 @@ const jwt = require("jsonwebtoken");
 const UserSchema = new mongoose.Schema({
   name: {
     type: String,
-    require: [true, "Please provide user's name"],
+    required: [true, "Please provide user's name"],
     minlength: 3,
     maxlength: 50,
   },
   email: {
     type: String,
-    require: [true, "Please provide user's email"],
+    required: [true, "Please provide user's email"],
     match: [
       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
       "Please provide valid email",
@@ -21,7 +21,7 @@ const UserSchema = new mongoose.Schema({
   },
   password: {
     type: String,
-    require: [true, "Please provide a password"],
+    required: [true, "Please provide a password"],
     minlength: 6,
     maxlength: 12,
   },
